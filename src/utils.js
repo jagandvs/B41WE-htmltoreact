ba5ff5bdd5bbfb173cbfe68acd53c97fb62cb406
jagandvs <dvsjmr9@gmail.com>
2023-02-04 11:58:28 +0530

    Intial commit

---
+++ src/utils.js
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    title: "card1",
+    imageUrl:
+      "https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    title: "Card2",
+    imageUrl:
+      "https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    title: "Card3",
+    imageUrl:
+      "https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    title: "Card4",
+    imageUrl:
+      "https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+];
